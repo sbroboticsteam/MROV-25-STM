b49f5fb02fd029886f6912cb75a148eb82c6b87e
Karamat <karamathasan420@gmail.com>
2026-04-15 17:54:38 -0400

    added flash and gitignore

---
+++ flash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# requires openOCD to flash
+set -e  
+
+mkdir -p build
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake
+
+cmake --build build
+
+ELF=$(find build -maxdepth 1 -name "*.elf" | head -n 1)
+if [[ ! -f "$ELF" ]]; then
+    echo "Error: ELF file not found in build folder"
+    exit 1
+fi
+echo "Flashing $ELF ..."
+
+openocd -f interface/stlink.cfg -f target/stm32f3x.cfg \
+    -c "program $ELF verify reset exit"
+
+echo "Flash Succesful !"
